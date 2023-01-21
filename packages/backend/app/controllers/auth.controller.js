0bcde3e2cf5ebb032a660a8aa5aa6950e5041a1b
zjy4fun <zjy4fun@gmail.com>
2023-01-21 12:05:34 +0800

    feature: add signin and signout controller logic

--- packages/backend/app/controllers/auth.controller.js
+++ packages/backend/app/controllers/auth.controller.js
@@ -47,10 +47,60 @@ exports.signup = async (req, res) => {
     }
 }
 
+exports.signin = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            }
+        });
 
+        if (!user) {
+            return res.status(404).send({message: "User not found."});
+        }
 
+        const passwordIsValid = bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
 
+        if (!passwordIsValid) {
+            return res.status(401).send({
+                message: "Invalid Password!",
+            });
+        }
 
+        const token = jwt.sign({id: user.id}, config.secret, {
+            expiresIn: 86400, // 24 hours
+        });
+        let authorities = [];
+        const roles = await user.getRoles();
+        for (let i = 0; i < roles.length; i++) {
+            authorities.push("ROLE_" + roles[i].name.toUpperCase());
+        }
+        req.session.token = token;
+
+        return res.status(200).send({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            roles: authorities,
+        });
+    } catch (error) {
+        return res.status(500).send({message: error.message});
+    }
+}
+
+exports.signout = async (req, res) => {
+    try {
+        req.session = null;
+        return res.status(200).send({
+            message: "You've been signed out!"
+        });
+    } catch (err) {
+        this.next(err);
+    }
+}
 
 
 
